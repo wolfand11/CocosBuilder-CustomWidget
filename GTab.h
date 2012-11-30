3371db7475c7f3b28e9f4cbe862e7c8b0c1e4b2e
GuoDong <smile_guodong@163.com>
2012-11-30 20:55:42 +0800

    1update CocosBuilder使用手册 2add Tab Widget

---
+++ GTab.h
@@ -0,0 +1,13 @@
+//
+//  GTab.h
+//  CocosBuilder
+//
+//  Created by GuoDong on 12-11-30.
+//
+//
+
+#import "CCMenu.h"
+
+@interface GTab : CCMenu
+
+@end
---
+++ GTab.m
@@ -0,0 +1,13 @@
+//
+//  GTab.m
+//  CocosBuilder
+//
+//  Created by GuoDong on 12-11-30.
+//
+//
+
+#import "GTab.h"
+
+@implementation GTab
+
+@end
