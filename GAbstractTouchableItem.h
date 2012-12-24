fd79f8f323168d8c69a0c3b0bc44ca832659e52f
GuoDong <smile_guodong@163.com>
2012-12-24 16:51:57 +0800

    添加GAbstractTouchableItem

---
+++ GAbstractTouchableItem.h
@@ -0,0 +1,13 @@
+//
+//  GAbstractTouchableItem.h
+//  CocosBuilder
+//
+//  Created by GuoDong on 12-12-24.
+//
+//
+
+#import "GAbstractTouchLayer.h"
+
+@interface GAbstractTouchableItem : GAbstractTouchLayer
+
+@end
---
+++ GAbstractTouchableItem.m
@@ -0,0 +1,13 @@
+//
+//  GAbstractTouchableItem.m
+//  CocosBuilder
+//
+//  Created by GuoDong on 12-12-24.
+//
+//
+
+#import "GAbstractTouchableItem.h"
+
+@implementation GAbstractTouchableItem
+
+@end
