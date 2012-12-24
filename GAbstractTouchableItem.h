6f8b2c71870e6b23dc5f682fa0891a2a8ee5764e
GuoDong <smile_guodong@163.com>
2012-12-24 16:53:50 +0800

    1添加新的GAbstractTouchableItem 2所有的RootNode都支持fullScreen 3如何在CocosBuilder中添加RootNode的操作文档。

--- GAbstractTouchableItem.h
+++ GAbstractTouchableItem.h
@@ -9,5 +9,9 @@
 #import "GAbstractTouchLayer.h"
 
 @interface GAbstractTouchableItem : GAbstractTouchLayer
-
+{
+    CGSize itemSize;
+}
+@property(readwrite,nonatomic,assign)CGSize itemSize;
+-(void) setItemSize:(CGSize)size;
 @end
--- GAbstractTouchableItem.m
+++ GAbstractTouchableItem.m
@@ -7,7 +7,32 @@
 //
 
 #import "GAbstractTouchableItem.h"
+#import "CocosBuilderAppDelegate.h"
+#import "PositionPropertySetter.h"
 
 @implementation GAbstractTouchableItem
+@synthesize itemSize;
 
+-(void) setItemSize:(CGSize)size
+{
+    // do nothing itemSize is equal to touchRectSize
+}
+
+-(void) setTouchRectSize:(CGSize)size
+{
+    [super setTouchRectSize:size];
+    [self updateItemSize];
+}
+
+-(void) adjustItemSize
+{
+    [PositionPropertySetter setSize:NSSizeFromCGSize(touchRectSize) forNode:[[CocosBuilderAppDelegate appDelegate] selectedNode] prop:@"itemSize"];
+}
+
+-(void) updateItemSize
+{
+    [self adjustItemSize];
+    [[CocosBuilderAppDelegate appDelegate] refreshProperty:@"position"];
+    [[CocosBuilderAppDelegate appDelegate] refreshProperty:@"touchRectOrigin"];
+}
 @end
