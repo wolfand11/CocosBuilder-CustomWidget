c0535dc13677a59630956552506e2aca6936bcfd
GuoDong <smile_guodong@163.com>
2013-01-05 22:29:59 +0800

    add LabelAtlas

---
+++ GCCLabelAtlas.h
@@ -0,0 +1,13 @@
+//
+//  GCCLabelAtlas.h
+//  CocosBuilder
+//
+//  Created by GuoDong on 13-1-5.
+//
+//
+
+#import "CCLabelAtlas.h"
+
+@interface GCCLabelAtlas : CCLabelAtlas
+
+@end
---
+++ GCCLabelAtlas.m
@@ -0,0 +1,13 @@
+//
+//  GCCLabelAtlas.m
+//  CocosBuilder
+//
+//  Created by GuoDong on 13-1-5.
+//
+//
+
+#import "GCCLabelAtlas.h"
+
+@implementation GCCLabelAtlas
+
+@end
