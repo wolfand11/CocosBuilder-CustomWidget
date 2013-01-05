bea691b01dc23588204f2e3f574223039286eceb
GuoDong <smile_guodong@163.com>
2013-01-05 22:30:51 +0800

    add LabelAtlas widget

--- GCCLabelAtlas.h
+++ GCCLabelAtlas.h
@@ -7,7 +7,19 @@
 //
 
 #import "CCLabelAtlas.h"
+#import "cocos2d.h"
 
-@interface GCCLabelAtlas : CCLabelAtlas
-
+@interface GCCLabelAtlas : CCNode
+{
+    CCSpriteFrame*  fontAtlas_;
+    NSUInteger      itemHeight_;
+    NSUInteger      itemWidth_;
+    NSUInteger      mapStartChar_;
+    NSString*       string_;
+}
+@property(readwrite,nonatomic,assign)NSUInteger     itemWidth;
+@property(readwrite,nonatomic,assign)NSUInteger     itemHeight;
+@property(readwrite,nonatomic,assign)NSUInteger     mapStartChar;
+@property(readwrite,nonatomic,assign)CCSpriteFrame* fontAtlas;
+@property(readwrite,nonatomic,retain)NSString*      string;
 @end
--- GCCLabelAtlas.m
+++ GCCLabelAtlas.m
@@ -7,7 +7,62 @@
 //
 
 #import "GCCLabelAtlas.h"
-
+#import "GCustomWidgetDefine.h"
+#define kDefaultItemSize    3
 @implementation GCCLabelAtlas
+@synthesize itemHeight;
+@synthesize itemWidth;
+@synthesize mapStartChar;
+@synthesize fontAtlas;
+@synthesize string;
 
+-(void) updateAtlasValues
+{
+    CGSize s = CGSizeMake([string_ length] * itemWidth_, itemHeight_);
+    [self setContentSize:s];
+}
+
+-(id) init
+{
+    if ([super init])
+    {
+        string_ = @"///";
+        return self;
+    }
+    return nil;
+}
+
+-(void) dealloc
+{
+    if (string_)
+    {
+        [string_ release];
+    }
+    [super dealloc];
+}
+
+-(void) setItemHeight:(NSUInteger)height
+{
+    itemHeight_ = height;
+    [self updateAtlasValues];
+}
+
+-(void) setItemWidth:(NSUInteger)width
+{
+    itemWidth_ = width;
+    [self updateAtlasValues];
+}
+
+-(void) setString:(NSString *)str
+{
+
+    NSAssert( str, @"Invalid string" );
+    
+    if( string_.hash != str.hash ) {
+        [string_ release];
+        string_ = [str copy];
+    }
+    
+    [self updateAtlasValues];
+}
 @end
