36c3cafe8ba025a4472541d8cf67ba04c3a939f4
GuoDong <smile_guodong@163.com>
2013-01-14 16:52:13 +0800

    add GConfigLayout

---
+++ GConfigLayout.h
@@ -0,0 +1,13 @@
+//
+//  GConfigLayout.h
+//  CocosBuilder
+//
+//  Created by GuoDong on 13-1-14.
+//
+//
+
+#import "GAbstractLayout.h"
+
+@interface GConfigLayout : GAbstractLayout
+
+@end
---
+++ GConfigLayout.m
@@ -0,0 +1,13 @@
+//
+//  GConfigLayout.m
+//  CocosBuilder
+//
+//  Created by GuoDong on 13-1-14.
+//
+//
+
+#import "GConfigLayout.h"
+
+@implementation GConfigLayout
+
+@end
