a700d9170131cc0cec1612f2da1c457ad6d82f2f
guodong <smile_guodong@163.com>
2014-01-24 21:50:48 +0800

    add GMenu widget

---
+++ Widget/GMenu.h
@@ -0,0 +1,13 @@
+//
+//  GMenu.h
+//  CocosBuilder
+//
+//  Created by guodong on 13-12-9.
+//
+//
+
+#import "GLayerPropertyExtension.h"
+
+@interface GMenu : GLayerPropertyExtension
+
+@end
---
+++ Widget/GMenu.m
@@ -0,0 +1,21 @@
+//
+//  GMenu.m
+//  CocosBuilder
+//
+//  Created by guodong on 13-12-9.
+//
+//
+
+#import "GMenu.h"
+
+@implementation GMenu
+-(id) init
+{
+    self = [super init];
+    if (!self) return NULL;
+    
+    self.mouseEnabled = NO;
+    
+    return self;
+}
+@end
